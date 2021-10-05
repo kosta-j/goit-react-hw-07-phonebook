6d2a2f6fe0dfbb913ad583498c33d9a82109e39f
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-10-05 08:40:08 +0300

    selectors memoization added

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact, fetchContact } from '../../Redux/operations';
-import {
-  getContacts,
-  getFilter,
-  getVisibleContacts,
-} from '../../Redux/selectors';
+import { getContacts, getVisibleContacts } from '../../Redux/selectors';
 import Notification from '../Notification/Notification';
 import ContactItem from './ContactItem';
 import s from './ContactList.module.css';
@@ -14,31 +10,32 @@ export default function ContactList() {
   const dispatch = useDispatch();
   useEffect(() => dispatch(fetchContact()), [dispatch]);
 
-  const filter = useSelector(getFilter);
-  const items = useSelector(getContacts);
-
-  const normalizedFilter = filter.toLowerCase();
-  const filteredContacts = items.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter),
-  );
+  const contacts = useSelector(getContacts);
+  const filteredContacts = useSelector(getVisibleContacts);
 
   const onDeleteBtnClick = contact => dispatch(deleteContact(contact.id));
 
-  return items.length < 1 ? (
+  return contacts.length < 1 ? (
     <Notification text="Contact list is empty" />
   ) : (
     <ul className={s.contactList}>
-      {filteredContacts.map(contact => (
-        <ContactItem
-          key={contact.id}
-          name={contact.name}
-          number={contact.number}
-        >
-          <button type={s.button} onClick={() => onDeleteBtnClick(contact)}>
-            Delete
-          </button>
-        </ContactItem>
-      ))}
+      {filteredContacts.length < 1 ? (
+        <Notification text={'Contacts not found'} />
+      ) : (
+        <>
+          {filteredContacts.map(contact => (
+            <ContactItem
+              key={contact.id}
+              name={contact.name}
+              number={contact.number}
+            >
+              <button type={s.button} onClick={() => onDeleteBtnClick(contact)}>
+                Delete
+              </button>
+            </ContactItem>
+          ))}
+        </>
+      )}
     </ul>
   );
 }
